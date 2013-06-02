6d70a913a1f557d567e751ac8357936b676d48cb
stuart bryce <stuart.bryce@sbsitech.com>
2013-06-02 23:03:28 +1000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyTut2::Application.config.secret_key_base = '2a707ae6e481a744b184b9f07046a20a9d7e0f88abac1026f9fe3a7a2d97f84ec8d6fce8d9b83d46a0bdd7cc9e2c8739f26d85298c901aef33794ea9e33f993d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyTut2::Application.config.session_store :cookie_store, key: '_RubyTut2_session'
